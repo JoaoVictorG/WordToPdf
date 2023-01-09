2c1bdc70dae221e35d436a32d155641c33f7290d
JoaoVictorG <joaovictorgon36@gmail.com>
2023-01-09 10:07:41 -0300

    Convertendo arquivo teste

---
+++ WordToPdf/WordToPdf/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using SautinSoft;
+
+namespace WordToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+            f.OpenPdf(@"C:\Users\joaov\Documents\TestePdf\Teste.pdf");
+
+            f.WordOptions.Format = SautinSoft.PdfFocus.CWordOptions.eWordDocument.Docx;
+            f.ToWord(@"C:\Users\joaov\Documents\TesteWord\Teste.docx");
+
+        }
+    }
+}
